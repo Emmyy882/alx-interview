ec2e61209a4812e06209a6ba589a8639451b56ca
Emmyy882 <emmaagbaje3@gmail.com>
2024-04-25 21:30:41 +0000

    fixed

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A python function used to rotate
+2D Matrix.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """A function to rotate 2D matrix in place.
+    """
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
